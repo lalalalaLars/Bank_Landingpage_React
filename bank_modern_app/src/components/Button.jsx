c73d1e694f725cbdd539f03b8c60251333635349
lalalalaLars <91459426+lalalalaLars@users.noreply.github.com>
2023-09-19 10:43:30 +0200

    first commit

---
+++ bank_modern_app/src/components/Button.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line react/prop-types
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles} `}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
